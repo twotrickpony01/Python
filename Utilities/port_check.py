5097ca51349e19b086dc546463e49c12d4777e55
Adam <adacjohn@gmail.com>
2024-01-13 16:50:56 -0500

    Cleanup

--- Utilities/port_check.py
+++ Utilities/port_check.py
@@ -1,12 +1,8 @@
 import socket
 
-def socket_check():
-    ip = raw_input("Please give an IP address to search: ")
-    port = int(raw_input("Please give a socket to check: "))
+def socket_check(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     if s.connect_ex((ip, port)):
         return "Port " + str(port) + " is open."
     else:
         return "Port " + str(port) + " is closed."
-
-print socket_check()
